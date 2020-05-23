e3e54f8ec5edb230d4babaabca178a8c4d7b1609
Adis Durakovic <adis.durakovic@gmail.com>
2020-05-23 22:01:07 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	. "github.com/logrusorgru/aurora"
+)
+
+func main() {
+
+	path, err := os.Getwd()
+	if err != nil {
+		log.Println(err)
+	}
+
+	input, err := ioutil.ReadFile(path + "/pubspec.yaml")
+	if err != nil {
+		fmt.Println(Red("Could not find pubspec.yaml"))
+		return
+	}
+
+	bumps := [5]string{"release", "major", "minor", "build"}
+	bump := ""
+	keepBuild := false
+
+	if len(os.Args) < 2 {
+		fmt.Println("Plese provide one of the bumps: ", Yellow(bumps))
+		return
+	} else {
+		bump = os.Args[1]
+		if !contains(bumps, bump) {
+			fmt.Println("Unknown bump! Only these are valid: ", Yellow(bumps))
+			return
+		}
+		if len(os.Args) > 2 && os.Args[2] == "build" {
+			keepBuild = true
+		}
+	}
+
+	lines := strings.Split(string(input), "\n")
+
+	for i, line := range lines {
+		if strings.HasPrefix(line, "version: ") {
+			lines[i] = bumpVersion(lines[i], bump, keepBuild)
+			break
+		}
+	}
+	output := strings.Join(lines, "\n")
+	err = ioutil.WriteFile("pubspec.yaml", []byte(output), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func contains(arr [5]string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
+
+func bumpVersion(v string, bump string, keepBuild bool) string {
+
+	regex := regexp.MustCompile(`[\r\n]+`) // windows newline fix
+	version := strings.Join(strings.Split(regex.ReplaceAllString(v, ""), "version: "), "")
+
+	checkBuild := strings.Split(version, "+")
+	hasBuild := len(checkBuild) == 2
+
+	numbers := strings.Split(checkBuild[0], ".")
+	vRelease, err := strconv.Atoi(numbers[0])
+	vMajor, err := strconv.Atoi(numbers[1])
+	vMinor, err := strconv.Atoi(numbers[2])
+	vBuild := 0
+	if hasBuild {
+		vBuild, err = strconv.Atoi(checkBuild[1])
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	switch bump {
+	case "release":
+		vRelease++
+		vMajor = 0
+		vMinor = 0
+		if !keepBuild {
+			vBuild = 0
+			hasBuild = false
+		}
+
+		break
+	case "major":
+		vMajor++
+		vMinor = 0
+		if !keepBuild {
+			vBuild = 0
+			hasBuild = false
+		}
+		break
+	case "minor":
+		vMinor++
+		if !keepBuild {
+			vBuild = 0
+			hasBuild = false
+		}
+		break
+	case "build":
+		vBuild++
+		break
+	}
+
+	rel := strconv.Itoa(vRelease)
+	maj := strconv.Itoa(vMajor)
+	min := strconv.Itoa(vMinor)
+	build := "+" + strconv.Itoa(vBuild)
+
+	if !hasBuild && bump != "build" {
+		build = ""
+	}
+
+	newVersion := rel + "." + maj + "." + min + build
+	fmt.Println("pubspec.yml bumped successfully from", Yellow(version), "->", Green(newVersion))
+	return "version: " + newVersion
+}
